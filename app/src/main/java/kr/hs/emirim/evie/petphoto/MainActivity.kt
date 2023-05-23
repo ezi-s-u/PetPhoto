b4d789151a9939a2224f4928f0e90f82244da174
ezi-s-u <ljsgoo0617@gmail.com>
2023-05-23 19:33:59 +0900

    직접풀어보기 4-4 시작함 Switch로 변경, 선택완료버튼 삭제

--- app/src/main/java/kr/hs/emirim/evie/petphoto/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/evie/petphoto/MainActivity.kt
@@ -5,39 +5,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
-    lateinit var checkStart : CheckBox
+    lateinit var switchStart: Switch
     lateinit var rg : RadioGroup
     lateinit var linear : LinearLayout
     lateinit var btnDone : Button
+    lateinit var btnFist : Button
     lateinit var imgv : ImageView
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkStart = findViewById(R.id.check_start)
+        switchStart = findViewById(R.id.switch_start)
         rg = findViewById(R.id.rg)
         linear = findViewById(R.id.linear)
         btnDone = findViewById(R.id.btn_done)
+        btnFist = findViewById(R.id.btn_first)
         imgv = findViewById(R.id.imgv)
         linear.visibility = View.INVISIBLE
 
-        checkStart.setOnCheckedChangeListener { compoundbutton, b ->
-            if(checkStart.isChecked) {
+        switchStart.setOnCheckedChangeListener { compoundbutton, b ->
+            if(switchStart.isChecked) {
                 linear.visibility = View.VISIBLE
             } else {
                 linear.visibility = View.INVISIBLE
             }
         }
 
-        btnDone.setOnClickListener {
+        rg.setOnCheckedChangeListener { compoundbutton, b ->
             when(rg.checkedRadioButtonId){
                 R.id.radio_dog -> imgv.setImageResource(R.drawable.dog) // img 변경할 때 사용하는 메서드
                 R.id.radio_cat -> imgv.setImageResource(R.drawable.cat)
                 R.id.radio_rabbit -> imgv.setImageResource(R.drawable.rabbit)
-                else -> Toast.makeText(applicationContext, "라디오버튼이 선택되지 않았습니다.",  Toast.LENGTH_SHORT).show() // show() : 화면에 보여줌
             }
         }
+
     }
 }
